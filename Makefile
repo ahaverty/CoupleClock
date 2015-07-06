83dcf6e7b15e83baa2acd34254fc878f99dfd2b2
Alan Haverty <alan@ahaverty.com>
2015-07-06 21:38:41 +0100

    Initial change from duplex clock to couple clock

--- Makefile
+++ Makefile
@@ -1,14 +1,9 @@
-export ARCHS = armv7 arm64
-
 include theos/makefiles/common.mk
 
-TWEAK_NAME = DuplexClock
-DuplexClock_FILES = Tweak.xm
-DuplexClock_CFLAGS = -fobjc-arc
+TWEAK_NAME = CoupleClock
+CoupleClock_FILES = Tweak.xm
 
 include $(THEOS_MAKE_PATH)/tweak.mk
 
 after-install::
 	install.exec "killall -9 SpringBoard"
-SUBPROJECTS += duplexclocksettings
-include $(THEOS_MAKE_PATH)/aggregate.mk
---
+++ coupleclocksettings/CoupleClockSettings.mm
@@ -0,0 +1,21 @@
+#import <Preferences/Preferences.h>
+
+@interface CoupleClockSettingsListController: PSListController {
+}
+@end
+
+@implementation CoupleClockSettingsListController
+
+-(NSArray *)timeZones {
+	return [NSTimeZone knownTimeZoneNames];
+}
+
+- (id)specifiers {
+
+	if(_specifiers == nil) {
+		_specifiers = [[self loadSpecifiersFromPlistName:@"CoupleClockSettings" target:self] retain];
+	}
+	
+	return _specifiers;
+}
+@end
